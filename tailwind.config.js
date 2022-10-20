70644db1b6ee6375526bfa9185a2a18349b2fae6
Sharon Lau <sharonytlau@gmail.com>
2022-10-20 17:20:29 +0800

    feat: self host font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Poppins'],
+    },
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
     extend: {},
   },
   plugins: [],
